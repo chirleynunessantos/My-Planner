6e99bc888d3ec76d5488a5bbb1d64530043fc055
chirley <chirleyfatec@gmail.com>
2025-09-30 17:21:42 -0300

    fix (agenda) ajuste na entidady agenda

--- src/main/java/br/com/myplanney/core/agenda/model/Agenda.java
+++ src/main/java/br/com/myplanney/core/agenda/model/Agenda.java
@@ -4,8 +4,12 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+import br.com.myplanney.core.enuns.Prioridade;
 import br.com.myplanney.core.tarefas.model.Tarefas;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Data;
@@ -15,9 +19,20 @@ import lombok.Data;
 @Builder*/
 public class Agenda {
 
-	LocalTime horario;
-	CategoriaAgenda categoria; 
-	String tarefa; // na tela aparecera lista que vira de tarfeas
-	String diaSemana;
-	LocalDateTime data; 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String titulo;
+	private String descricao;
+	private LocalTime dataInicio;
+	private String local;
+	private String categoria;
+	private Prioridade prioridade;
+	private String status;
+	private String diaSemana;
+	private String recorrencia; // tera um botao... ai clicar no botar o usuario tera como informar em quais dias
+	// essa tarefa ira se repetir e o sistema ira popular sozinho essas datas
+	
+	
+	//terei dois template de agenda uma em lista e outra em calendario
 }
